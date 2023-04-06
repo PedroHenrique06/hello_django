81cfe1ff74443b75ed654beb161663c9bd6b5cdd
Pedro Henrique <pedrosales2806@gmail.com>
2023-04-06 19:30:22 -0300

    Adição da função de subtração

--- core/views.py
+++ core/views.py
@@ -4,18 +4,26 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# Mostra uma mensagem padrão
 def hello(request):
     return HttpResponse(f"<h1>Hello World!!!</h1>")
 
 
+# Soma dois números e retorna o resultado
 def sum(request, x, y):
     return HttpResponse(f"<h1>a soma de {x} e {y} é {x+y}</h1>")
 
 
+# Subtrai um número do outro
+def sub(request, x, y):
+    return HttpResponse(f"<h1>a soma de {x} e {y} é {x-y}</h1>")
+
+
+# Multiplica dois números e retorna o resultado
 def multi(request, x, y):
     return HttpResponse(f"<h1>a soma de {x} e {y} é {x*y}</h1>")
 
 
+# Divide dois números e retorna o resultado
 def div(request, x, y):
     return HttpResponse(f"<h1>a soma de {x} e {y} é {x/y}</h1>")
-
